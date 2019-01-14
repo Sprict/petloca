14fe9a046875d778920cee2f36fdd32babcd269d
奥田蓮 <sprict@okudarennoMacBook-puro.local>
2019-01-14 15:41:00 +0900

    init

---
+++ petloca/petloca/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  petloca
+//
+//  Created by 奥田蓮 on 2019/01/14.
+//  Copyright © 2019 奥田蓮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
