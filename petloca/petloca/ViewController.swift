60fe4864a226adb509b661660b8f7e564ce80635
奥田蓮 <sprict@okudarennoMacBook-puro.local>
2019-01-28 20:44:29 +0900

    要素を追加しました。

--- petloca/petloca/ViewController.swift
+++ petloca/petloca/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLocationManager()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else {return}
+        
+    locationManager.requestWhenInUseAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    @IBOutlet weak var MapView: MKMapView!
 }
 
